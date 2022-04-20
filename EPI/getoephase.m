10835dc0d8ded8f5fb66e508d78651619349e574
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-04-20 17:08:49 -0400

    On branch main 	modified:   getoephase.m

--- EPI/getoephase.m
+++ EPI/getoephase.m
@@ -7,25 +7,46 @@ function th = getoephase(x)
 % Inputs
 %  x    [nx etl nCoils]   EPI echo train of length 'etl' without phase-encoding.
 
+verbose = true;
+
 [nx etl nCoils] = size(x);
 
-%th = zeros(nx,1);  % odd/even echo phase difference
-% th = th + abs(tmp).^2.*exp(1i*angle(xtmp(:,2:2:,ic)./xtmp(
+etl = size(x, 2);
+
 
 % Estimate off-resonance phase from even echoes (center line)
 % Only need one 'slice'
 % Magnitude-squared coil weighting as in phase-contrast MRI
+% Fit straight line and subtract from all echoes
 th = zeros(1, etl/2);
 for ic = 1:nCoils
-    xe = x(end/2,2:2:end,ic);  % even echoes
+    xe = x(end/2,2:2:etl,ic);  % even echoes
     tmp = unwrap(angle(xe));
-    tmp = tmp - mean(tmp);
+    tmp = tmp - tmp(end/2);  % need common reference since we're combining coils
     th = th + abs(xe).^2 .* exp(1i*tmp);
 end
+if etl/2 < 10 
+    maSpan = 1;
+else
+    maSpan = 5;   % moving-average span for smoothing
+end
+th = smooth(unwrap(angle(th)), maSpan);
+X = [1:2:etl]';
+B = [ones(length(th),1) X];
+a = B\th(:);  % a(2) = off-resonance phase accrual per echo (radians)
+XY = ones(nx,1) * [1:etl] - 1;  % [nx etl]
+DPH = a(2)*repmat(XY, [1 1 nCoils]);  % linear fit to off-resonance phase accrual (radians)
+xc = x.*exp(-1i*DPH);  % 'c' for 'corrected'
 
-th = angle(th);
-H = [ones(sum(mask(:)),1) X(mask)];  % spatial basis matrix (2d linear)
-ph(isl,:) = H\th(mask);  
+if verbose
+    figure; subplot(121); im(angle(x(:,:,14)));
+    subplot(122); im(angle(xc(:,:,14)));
+    figure; ic = 14;
+    plot(angle(x(end/2,:,ic)),'r'); hold on
+    plot(angle(xc(end/2,:,ic)), 'g'); hold on
+end
+
+keyboard
 
 return
 
