9867c2c7e6c58e90d5d6437b9aa23accac661631
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-11-03 15:19:42 -0400

    Added NUFFT gridding option On branch main 	modified:   rampsamp2cart.m

--- +hmriutils/+epi/rampsamp2cart.m
+++ +hmriutils/+epi/rampsamp2cart.m
@@ -1,4 +1,4 @@
-function dc = rampsamp2cart(dr, kx, nx, fov)
+function dc = rampsamp2cart(dr, kx, nx, fov, method)
 %
 % Interpolate ramp-sampled data (e.g., EPI) to Cartesian grid.
 %
@@ -7,16 +7,27 @@ function dc = rampsamp2cart(dr, kx, nx, fov)
 %  kx      [nr 1]      k-space sampling locations (cycles/cm)
 %  nx      [1]         Number of pixels (along x) in reconstructed image
 %  fov     [1]         FOV (cm) along x
+%  method  string      'spline' (default) or 'nufft' (accurate, but slower)
 %
 % Output:
 %  dc      [nx, ...]   Data interpolated onto Cartesian grid along x
 %
 % To test:
-% >> rampsamp2cart('test');
+% >> rampsamp2cart('test', 'spline');
 
 if strcmp(dr, "test")
-	sub_test;
+    if ~exist('kx', 'var')  % 2nd argument serves dual purpose
+        method = 'spline';
+    else
+        method = kx;
+    end
+    
+	sub_test(method);
 	return
+end
+
+if ~exist('method', 'var')
+    method = 'spline';
 end
 
 nd = ndims(dr);
@@ -31,53 +42,53 @@ if nr ~= size(kx,1)
     error('Length of k-space and data (along 1st dimenstion) must match');
 end
 
-% Interpolate to Cartesian grid
-osf = 2;   % oversampling factor
-kxc = [ (-nx/2+1/(2*osf)):(1/osf):(nx/2-1/(2*osf)) ] * 1/fov;  % cycles/cm
-d1 = interp1(kx, dr, kxc, 'spline', 'extrap');
+if strcmp(method, 'spline')
+    % Interpolate to Cartesian grid
+    osf = 2;   % oversampling factor
+    kxc = [ (-nx/2+1/(2*osf)):(1/osf):(nx/2-1/(2*osf)) ] * 1/fov;  % cycles/cm
+    d1 = interp1(kx, dr, kxc, 'spline', 'extrap');
 
-% crop fov
-x1 = fftshift(ifft(fftshift(d1,1), [], 1),1);
-x1 = x1( (nx-nx/2+1):(nx+nx/2), :);
+    % crop fov
+    x1 = fftshift(ifft(fftshift(d1,1), [], 1),1);
+    x1 = x1( (nx-nx/2+1):(nx+nx/2), :);
 
-% Deapodize
-dr2 = 0*dr(:,1,1);
-dr2(round(end/2)+1) = 1;  % impulse
-d2 = interp1(kx, dr2, kxc, 'spline', 'extrap');
-ap = abs(fftshift(ifft(fftshift(d2))))'; % image-space apodization
-ap = ap((nx-nx/2+1):(nx+nx/2));
-ap = ap/max(ap);
-x1 = bsxfun(@times, 1./ap, x1);
+    % Deapodize
+    dr2 = 0*dr(:,1,1);
+    dr2(round(end/2)+1) = 1;  % impulse
+    d2 = interp1(kx, dr2, kxc, 'spline', 'extrap');
+    ap = abs(fftshift(ifft(fftshift(d2))))'; % image-space apodization
+    ap = ap((nx-nx/2+1):(nx+nx/2));
+    ap = ap/max(ap);
+    x1 = bsxfun(@times, 1./ap, x1);
 
-% Back to k-space
-x1 = reshape(x1, [nx drSize(2:end)]);
-dc = fftshift(fft(fftshift(x1,1), [], 1),1);
+    % Back to k-space
+    x1 = reshape(x1, [nx drSize(2:end)]);
+    dc = fftshift(fft(fftshift(x1,1), [], 1),1);
+else
+    % The following uses NUFFT and avoids apodization, but is ~20x slower
 
-return
+    % reshape to 2D for looping
+    dr = reshape(dr, nr, []);
+    nm = size(dr,2);
 
-% The following uses NUFFT and avoids apodization, but is ~20x slower
+    % Get Gmri object
+    [~, A, dcf] = hmriutils.epi.reconecho([], nx, [], [], kx, fov);
 
-% reshape to 2D for looping
-dr = reshape(dr, nr, []);
-nm = size(dr,2);
+    % Interpolate to Cartesian grid
+    dc = zeros(nx, nm);  % Cartesian data
+    for ii = 1:nm
+        x = hmriutils.epi.reconecho(dr(:,ii), nx, A, dcf);
+        dc(:,ii) = fftshift(fft(fftshift(x,1), [], 1),1);
+    end
 
-% Get Gmri object
-[~, A, dcf] = reconecho([], nx, [], [], kx, fov);
-
-% Interpolate to Cartesian grid
-dc = zeros(nx, nm);  % Cartesian data
-for ii = 1:nm
-    x = reconecho(dr(:,ii), nx, A, dcf);
-    dc(:,ii) = fftshift(fft(fftshift(x,1), [], 1),1);
+    % Reshape
+    dc = reshape(dc, [nx drSize(2:end)]);
 end
 
-% Reshape
-dc = reshape(dc, [nx drSize(2:end)]);
-
 return
 
 
-function sub_test
+function sub_test(method)
 
 % test object
 nx = 128;
@@ -106,8 +117,8 @@ mask = true(nx,1);
 A = Gmri([fov*kx(:)],mask,'nufft',nufft_args);
 yr = A*x;
 
-% Interpolated onto Cartesian grid 
-yc = hmriutils.epi.rampsamp2cart(yr, kx, nx, fov);
+% Interpolate onto Cartesian grid 
+yc = hmriutils.epi.rampsamp2cart(yr, kx, nx, fov, method);
 
 % Do IFT and compare with true object
 xhat = fftshift(ifft(fftshift(yc,1), [], 1),1);
