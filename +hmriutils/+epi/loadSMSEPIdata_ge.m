1b4df060f3492489e1a0551927b868472d23dd61
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-09-24 15:57:22 -0400

    On branch main 	modified:   loadSMSEPIdata_ge.m

--- +hmriutils/+epi/loadSMSEPIdata_ge.m
+++ +hmriutils/+epi/loadSMSEPIdata_ge.m
@@ -1,10 +1,37 @@
-function dat = loadSMSEPIdata_ge(pfile, frame, etl, nz, mb)
+function dat = loadSMSEPIdata_ge(pfile, frame, etl, nz, mb, readoutfile)
 %
-% Load raw data acquired with smsepi.seq on GE scanners.
-% Assumes that smsepi.seq is created with SMS-EPI/sequence/Pulseq/writeSMSEPI.m
+% Load ramp-sampled EPI data acquired with smsepi.seq on GE scanners,
+% and interpolate onto cartesian grid.
+% Assumes that smsepi.seq was created with SMS-EPI/sequence/Pulseq/writeSMSEPI.m
+%
+% Inputs
+% ------
+% pfile         File location of the P-file containing the SMS-EPI data
+% frame         Temporal frame to read
+% etl           echo train length
+% nz            number of slices in reconstructed image volume
+% mb            SMS/multiband factor
+% readoutfile   Name of .mod file containing the EPI echo trapezoid waveform.
 %
 % Output
-%    dat    [nx etl npartitions ncoils]
+% ------
+% data         k-space data for the given frame
+%                Size: [nx etl npartitions ncoils], where npartitions = nz/mb, 
+%                      i.e., the number of slices in the full reconstructed volume
+%                      divided by the multiband factor
+%
+% Outputs
+% -------
+% data         k-space data for the given frame
+%                Size: [nsamples, ncoils, npartitions], where npartitions is
+%                      the number of slices in the full reconstructed volume
+%                      divided by the multiband factor
+
+% mask         k-space CAIPI sampling mask
+%                Size: [nx, ny, mb] Note that this mask is the same for each set
+%                      of simultaneously excited slices. Also note that
+%                      nsamples = sum(mask(:))
+
 
 % Load data for one frame
 % nfid = number of ADC samples per echo (>nx due to ramp sampling)
@@ -24,18 +51,20 @@ d = reshape(d, nfid, ncoils, etl, npartitions);
 
 d = permute(d, [1 3 4 2]);   % [nfid etl npartitions ncoils]
 
-% Get kspace locations and interpolate onto Cartesian grid
-% Apply 1/2 sample shift to align echoes.
-%system('tar xf smsepi.tar module3.mod');
-[rf,gx,gy,gz,desc,paramsint16,paramsfloat,hdr] = toppe.readmod('readout.mod');
-kx = sysGE.raster*1e-6*sysGE.gamma*1e-4*cumsum(gx);  % cycles/cm
-kx = kx - kx(end)/2;
-kx = kx((hdr.npre+1):(hdr.npre+size(d,1))); % dwell time = 4us = gradient raster time, so number of samples = hdr.rfres here
-del = 0.0;
-kxo = interp1(1:nfid, kx, (1:nfid) - 0.5 - del, 'linear', 'extrap');
-kxe = interp1(1:nfid, kx, (1:nfid) + 0.5 + del, 'linear', 'extrap');
-kxe = fliplr(kxe);
+% Get kspace locations (ramp sampled).
+% 
+[rf,gx,gy,gz,desc,paramsint16,paramsfloat,hdr] = toppe.readmod(readoutfile);
+gamma = 4.2576;      % kHz/Gauss
+dt = 4e-3;   % GE gradient raster time, ms
+kx = gamma*dt*cumsum(gx);   % cycles/cm
+kx_ctr = mean(kx(end/2:(end/2)+1));
+kx = kx - kx_ctr;    % assume that echo is sampled symmetrically about kx=0
+kx = kx((hdr.npre+1):(hdr.npre+hdr.rfres));    % ADC on during this portion of the waveform
+dur = numel(kx)*dt;
+kxo = interp1([0 dt:dt:dur], [0 kx(:)'], dt/2:dt/2:dur);  % odd echoes
+kxe = fliplr(kxo);
 
+% Interpolate onto Cartesian grid
 fprintf('Interpolating... ');
 fovXcm = fov(1)*100;
 dco = hmriutils.epi.rampsamp2cart(d(:, 1:2:end, :, :), kxo, nx, fovXcm, 'nufft');   % odd echoes
