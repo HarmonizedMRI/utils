83beea34744df6e86611d4249b6eccf5c87fcaf4
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-12-15 18:24:46 -0500

    On branch develop 	modified:   getsliceordering.m

--- +hmriutils/+epi/getsliceordering.m
+++ +hmriutils/+epi/getsliceordering.m
@@ -1,7 +1,7 @@
-function IZ_start = getsliceordering(np)
+function Z_start = getsliceordering(np)
 % Start slice order for the np partitions in SMS-EPI
 % Interleaved, with last two shots/partitions swapped
 
-IZ_start = [1:2:np 2:2:np];
-IZ_start = [IZ_start(1:end-2) IZ_start(end) IZ_start(end-1)]; 
+Z_start = [1:2:np 2:2:np];
+Z_start = [Z_start(1:end-2) Z_start(end) Z_start(end-1)]; 
 
