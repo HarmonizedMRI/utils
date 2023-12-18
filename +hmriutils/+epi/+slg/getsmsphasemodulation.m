d2ef6699c4411bbab8fd08caa76b901139d0da85
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-12-17 20:58:41 -0500

    On branch develop 	modified:   getsmsphasemodulation.m

--- +hmriutils/+epi/+slg/getsmsphasemodulation.m
+++ +hmriutils/+epi/+slg/getsmsphasemodulation.m
@@ -1,4 +1,4 @@
-function ph = getsmsphasemodulation(nz, IZ, mask)
+function ph = getsmsphasemodulation(nz, Z, smask)
 %
 % Get phase modulation due to gz blips along the EPI train.
 % This is used to shift reconstructed slices back to center of FOV
@@ -6,22 +6,22 @@ function ph = getsmsphasemodulation(nz, IZ, mask)
 %
 % Inputs
 %   nz      [1]             Number of slices in full image volume
-%   IZ      [mb]            SMS slice indices in full image volume
-%   mask    [nx ny mb]      Sampled (3D) k-space locations along echo train,
+%   Z       [mb]            SMS slice indices in full image volume
+%   smask   [nx ny mb]      Sampled (3D) k-space locations along echo train,
 %                           which defines the z blips. See getsamplingmask.m
 % Output
 %   ph      [ny mb]         Phase modulation
 
-[nx ny mb] = size(mask);
+[nx ny mb] = size(smask);
 
-assert(length(IZ) == mb, 'Number of slices (IZ) and size(mask,3) must match');
+assert(length(Z) == mb, 'Number of slices (Z) and size(smask,3) must match');
 
-% 'Excite' one slice at a time and apply z blips as defined in 'mask',
+% 'Excite' one slice at a time and apply z blips as defined in 'smask',
 % and get resulting phase modulation 
 d_ex_1 = zeros(nx, ny, mb);
 for z = 1:mb
     d_ex_1(:,:,z) = 1;  % 'excite' a slice
-    [~, tmp] = hmriutils.epi.slg.simulatesmsepishot(d_ex_1, nz, IZ, mask);
+    [~, tmp] = hmriutils.epi.slg.simulatesmsepishot(d_ex_1, nz, Z, smask);
     ph(:,z) = tmp(:,z);
     d_ex_1(:,:,z) = 0;  % undo to get ready for next slice
 end
