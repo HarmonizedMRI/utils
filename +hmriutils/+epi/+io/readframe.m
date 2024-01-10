34aadfc50a0cf887cee8370e3b31de868e1d3628
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2024-01-09 21:39:39 -0500

    On branch develop 	modified:   readframe.m

--- +hmriutils/+epi/+io/readframe.m
+++ +hmriutils/+epi/+io/readframe.m
@@ -1,4 +1,7 @@
 function d = readframe(fn, frame)
+%
+% Load one frame of raw data form .h5 file created with
+% draw2hdf.m
 
 sz = h5info(fn, '/kdata/real').Dataspace.Size;
 dr = h5read(fn, '/kdata/real', [1 1 1 1 frame], [sz(1:4) 1]);
