07918c5ce14d61a04393298d0fb517b03d3d5893
YongliHe23 <yonglihe@umich.edu>
2025-12-10 15:24:01 -0500

    generalize to in-plane accelerated cases

--- +hmriutils/+epi/getsamplingmask.m
+++ +hmriutils/+epi/getsamplingmask.m
@@ -1,4 +1,4 @@
-function smask = getsamplingmask(ikz, nx, ny, mb)
+function smask = getsamplingmask(ikz, nx, ny, mb,Ry)
 % Define SMS EPI sampling mask
 %
 % Input
@@ -9,18 +9,22 @@ function smask = getsamplingmask(ikz, nx, ny, mb)
 %
 % Optional input
 %   mb   [1]     SMS multiband factor. If not provided, assume mb = length(ikz)
+%   Ry   [1]     in-plane undersample factor
 %
 % Output
 %   smask    [nx ny mb]     3D k-space sampling mask (maskyz repeated at each kx point)
 
 if nargin < 4
     mb = length(ikz);
+    Ry=1;
+elseif nargin < 5
+    Ry = 1;
 end
 
 if length(ikz) == mb
     mb = length(ikz);
     tmp = [];
-    for ii = 1:ny/mb
+    for ii = 1:Ry:Ry*ny/mb
         tmp = [tmp ikz(:)'];
     end
     ikz = tmp;
@@ -28,9 +32,9 @@ else
     assert(~mod(ny, length(ikz)), 'ny must be a multiple of multiband factor (mb)');
 end
 
-smaskyz = false(ny,mb);
-for y = 1:ny
-    smaskyz(y, ikz(y)) = true;
+smaskyz = false(Ry*ny,mb);
+for y = 1:Ry:Ry*ny
+    smaskyz(y, ikz(ceil(y/Ry))) = true;
 end
-smask = false(nx, ny, mb);
+smask = false(nx, Ry*ny, mb);
 smask(:, smaskyz(:,1:mb)) = true;
