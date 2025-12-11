52c62217cc3296e2d932337d0dcdbf61c528e146
YongliHe23 <yonglihe@umich.edu>
2025-12-11 12:26:45 -0500

    update getsliceordering for np is odd case

--- +hmriutils/+epi/getsliceordering.m
+++ +hmriutils/+epi/getsliceordering.m
@@ -1,7 +1,9 @@
 function Z_start = getsliceordering(np)
 % Start slice order for the np partitions in SMS-EPI
-% Interleaved, with last two shots/partitions swapped
+% Interleaved, with last two shots/partitions swapped if np is even
 
 Z_start = [1:2:np 2:2:np];
-Z_start = [Z_start(1:end-2) Z_start(end) Z_start(end-1)]; 
+if ~mod(np,2)
+    Z_start = [Z_start(1:end-2) Z_start(end) Z_start(end-1)]; 
+end
 
